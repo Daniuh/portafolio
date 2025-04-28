057f8ea29146252a03845398dc893bd3c2e75f2e
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-04-28 10:11:47 +0200

    Se organiza diseño de la página en general para poder navegar correctamente y se organiza diseño del navegador

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,24 @@ import { loadHome, loadNavigator } from '../components/index.js';
 import '../styles/style.css';
 
 document.querySelector('#app').innerHTML = `
-  <div id="home"></div>
-  <div id="navigator"></div>
+<div id="navigator"></div>
+<main>
+  <section id="home" class="w-full min-h-screen">
+    <div id="home"></div>
+  </section>
+
+  <section id="about" class="w-full min-h-screen">
+    <!-- contenido de Sobre mí -->
+  </section>
+
+  <section id="projects" class="w-full min-h-screen">
+    <!-- contenido de Proyectos -->
+  </section>
+
+  <section id="contact" class="w-full min-h-screen">
+    <!-- contenido de Contacto -->
+  </section>
+</main>
 `;
 
 const element = document.querySelector('#container');
