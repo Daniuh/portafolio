212c8f5cadb449d668b2272bfa65318896562dbf
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-04-29 12:19:31 +0200

    Se finaliza el diseño parcial del aboutMe

--- src/components/aboutMe-page/aboutMe-page.js
+++ src/components/aboutMe-page/aboutMe-page.js
@@ -1,7 +1,26 @@
 import '../../styles/style.css';
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 export async function loadAboutMe() {
   const response = await fetch('/aboutMe-page/aboutMe-page.html');
   const html = await response.text();
   document.getElementById('about').innerHTML = html;
+
+    new Splide('.splide', {
+      type: 'loop',
+      perPage: 3,
+      autoplay: true, 
+      interval: 3000,  
+      pauseOnHover: true,
+      breakpoints: {
+        768: {
+          perPage: 1,
+        },
+        1024: {
+          perPage: 2,
+        },
+      },
+    }).mount();
 }
+
