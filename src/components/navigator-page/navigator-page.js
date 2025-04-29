1af56dee72d4a1af18736d2a5555c9954b64aead
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-04-29 07:43:09 +0200

    Se crea diseño interactivo para el usuario en el menú

--- src/components/navigator-page/navigator-page.js
+++ src/components/navigator-page/navigator-page.js
@@ -5,3 +5,26 @@ export async function loadNavigator() {
   const html = await response.text();
   document.getElementById('navigator').innerHTML = html;
 }
+
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const links = document.querySelectorAll('.menu-link');
+
+  let currentSectionId = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      currentSectionId = section.getAttribute('id');
+    }
+  });
+
+  links.forEach(link => {
+    link.classList.remove('border-b-2', 'border-yellow-300', 'text-yellow-300');
+    if (link.getAttribute('href') === `#${currentSectionId}`) {
+      link.classList.add('border-b-2', 'border-yellow-300', 'text-yellow-300');
+    }
+  });
+});
